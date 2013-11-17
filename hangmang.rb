7ee1658c9502a1098226ffb955497cfc6b41d15c
John Moon <johnmoonyy@gmail.com>
2013-11-17 17:21:20 -0500

    First refractor and clean up of previous code. No new code was added

--- hangmang.rb
+++ hangmang.rb
@@ -1,59 +1,78 @@
-puts "Welcome to Hangman!"
-
-  random_word = ["array"]
-  guess_word = random_word.sample
-  guess_left = 10 
-
-  letter_count = guess_word.split("").count
-  hidden = []
-  hidden << ("_" * letter_count) 
-
-  answer = []
-    letter_count.times do 
-    answer << "_"
-  end
-
-  puts "Word: #{answer}(#{letter_count.size}): " 
-  puts "Guesses remaining: #{guess_left}"
+def guess_input
   print "Guess a single letter (a-z) or the entire word: "  
-  guess = gets.chomp
+  @guess = gets.chomp
+end
 
-  while guess_left != 0  
-    if guess.size > 1
-      if guess == guess_word
-        puts
-        puts "Congradulations you\'ve guessed the word!" 
-        exit
-      else
-        puts
-        puts "You guess the wrong word. You lose."
-        exit
-      end
+def ending_message
+  if @guess == @guess_word
+    puts
+    puts "Congradulations you\'ve guessed the word!" 
+    exit
+  else
+    puts
+    puts "You guess the wrong word. You lose."
+    exit
+  end
+end
+
+def guess_word
+  random_word = ["array"]
+  @guess_word = random_word.sample
+  @guess_left = 10 
+end
+
+def game_loop
+  while @guess_left != 1  
+    if @guess.size > 1
+      ending_message
     else
-      if guess_word.include?(guess) 
-          i = guess_word.index(guess)
-        guess_word.count(guess).times do 
-          value = guess_word.index(guess, i) 
-          answer[value] = guess
+      if @guess_word.include?(@guess) 
+          i = @guess_word.index(@guess)
+        @guess_word.count(@guess).times do 
+          value = @guess_word.index(@guess, i) 
+          @answer[value] = @guess
           i += 1 
         end
-        if answer.join('') == guess_word
+        if @answer.join('') == @guess_word
           puts
           puts "Congradulations you\'ve guessed the word!" 
           exit
         end
         puts
-        puts "Found #{guess_word.count(guess)} occurence(s) of the character #{guess}"
+        puts "Found #{@guess_word.count(@guess)} occurence(s) of the character #{@guess}"
         puts
       else
         puts
         puts "Sorry, no letters found"
         puts
-        guess_left -= 1
+        @guess_left -= 1
       end
     end
-    puts "Word: #{answer}"
-    puts "Guesses remaining: #{guess_left}"
-    print "Guess a single letter (a-z) or the entire word: "  
-    guess = gets.chomp
+    puts "Word: #{@answer}"
+    puts "Guesses remaining: #{@guess_left}"
+    guess_input 
   end
+end
+
+def create_hidden_format_of_answer
+  @letter_count = @guess_word.split("").count
+  @hidden = []
+  @hidden << ("_" * @letter_count) 
+
+  @answer = []
+    @letter_count.times do 
+    @answer << "_"
+  end
+end
+
+
+puts "Welcome to Hangman!"
+
+  guess_word
+  create_hidden_format_of_answer
+
+  puts "Word: #{@answer}(#{@letter_count.size}): " 
+  puts "Guesses remaining: #{@guess_left}"
+  
+  guess_input 
+  game_loop 
